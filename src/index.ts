fb3e28d5e0c3260d30778dee56312fe48499e14f
Zia Khan <ziakhanfr5@gmail.com>
2026-06-14 06:33:00 +0500

    Remove unused flags from index.ts
    Removed unnecessary flags from the configuration.

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,7 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
+            
         ]
     },
     authStrategy: new LocalAuth({
