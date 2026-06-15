c945a067eba7ea4bd414a4e647b321c5e35183ca
Zia Khan <ziakhanfr5@gmail.com>
2026-06-15 11:04:52 +0500

    Fix moderation response data assignment

--- src/handlers/moderation.ts
+++ src/handlers/moderation.ts
@@ -14,7 +14,7 @@ const moderateIncomingPrompt = async (prompt: string) => {
 		input: prompt
 	});
 
-	const moderationResponseData = moderationResponse.data;
+	const moderationResponseData = moderationResponse;
 	const moderationResponseCategories = moderationResponseData.results[0].categories;
 	const blackListedCategories = config.promptModerationBlacklistedCategories;
 
