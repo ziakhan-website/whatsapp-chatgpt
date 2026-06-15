53f879f0f0da4b8d9ec1ae195ffaf464af96f4d9
Zia Khan <ziakhanfr5@gmail.com>
2026-06-15 06:31:41 +0500

    Remove '--single-process' flag from index.ts
    Removed the '--single-process' flag from the options.

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ const client = new Client({
         '--disable-accelerated-2d-canvas',
         '--no-first-run',
         '--no-zygote',
-        '--single-process',
+        
         '--disable-gpu',
         '--disable-web-security',
         '--disable-features=VizDisplayCompositor'
